cd69f80e8244779cf7931b1f6424927838762a0f
cmd58 <cmd58@njit.edu>
2022-03-30 17:59:14 -0400

    Updated Carousel

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -9,7 +9,10 @@ def test_request_main_menu_links(client):
     assert b'<a class="nav-link" href="/page2">Docker</a>' in response.data
     assert b'<a class="nav-link" href="/page3">Python & Flask</a>' in response.data
     assert b'<a class="nav-link" href="/page4">CI/CD</a>' in response.data
-
+    assert b'<a class="nav-link" href="/page5">Pylint</a>' in response.data
+    assert b'<a class="nav-link" href="/page6">AAA Testing</a>' in response.data
+    assert b'<a class="nav-link" href="/page3">OOP</a>' in response.data
+    assert b'<a class="nav-link" href="/page4">SOLID</a>' in response.data
 
 def test_request_about(client):
     """This makes the index page"""
@@ -40,6 +43,34 @@ def test_request_page4(client):
     response = client.get("/page4")
     assert response.status_code == 200
     assert b"CI/CD" in response.data
+
+
+def test_request_page1(client):
+    """This makes the index page"""
+    response = client.get("/page5")
+    assert response.status_code == 200
+    assert b"Pylint" in response.data
+
+
+def test_request_page2(client):
+    """This makes the index page"""
+    response = client.get("/page6")
+    assert response.status_code == 200
+    assert b"AAA Testing" in response.data
+
+
+def test_request_page3(client):
+    """This makes the index page"""
+    response = client.get("/page7")
+    assert response.status_code == 200
+    assert b"OOP" in response.data
+
+
+def test_request_page4(client):
+    """This makes the index page"""
+    response = client.get("/page8")
+    assert response.status_code == 200
+    assert b"SOLID" in response.data
 
 def test_request_page_not_found(client):
     """This makes the index page"""
